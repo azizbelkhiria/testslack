46146759a7a193e25ce4aa8876ed1bc05851417e
azizbelkhiria <80126484+azizbelkhiria@users.noreply.github.com>
2023-05-19 03:43:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,12 @@ from langchain.chains.conversation.memory import (
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 #Langchain implementation
-template = """Assistant is a large language model trained by OpenAI.
-
-    Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-
-    Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-
-    Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-
-    {history}
+template = """
     Human: {human_input}
     Assistant:"""
 
 prompt = PromptTemplate(
-    input_variables=["history", "human_input"], 
+    input_variables=["human_input"], 
     template=template
 )
 
@@ -32,7 +24,7 @@ chatgpt_chain = LLMChain(
     llm=OpenAI(temperature=0), 
     prompt=prompt, 
     verbose=True,
-    memory=ConversationBufferWindowMemory(k=2),
+    memory=ConversationBufferWindowMemory(k=1),
 )
 
 #Message handler for Slack
