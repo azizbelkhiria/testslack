f2dc8bff59948b059b060fc336db0305589fdac0
azizbelkhiria <80126484+azizbelkhiria@users.noreply.github.com>
2023-05-19 03:57:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
 from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
-from langchain.chains.conversation.memory import (
-    ConversationBufferWindowMemory,
-)
+from langchain.chains.conversation.memory import (ConversationBufferWindowMemory,)
+
+# new code
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -19,24 +23,34 @@ prompt = PromptTemplate(
     template=template
 )
 
+# new code
+root_dir = "./"
 
+pdf_folder_path = f'{root_dir}/pdfs/'
+os.listdir(pdf_folder_path)
+
+# location of the pdf file/files. 
+loaders = [UnstructuredPDFLoader(os.path.join(pdf_folder_path, fn)) for fn in os.listdir(pdf_folder_path)]
+CHESSGPT = VectorstoreIndexCreator().from_loaders(loaders)
+
+"""
 chatgpt_chain = LLMChain(
     llm=OpenAI(temperature=0), 
     prompt=prompt, 
     verbose=True,
     memory=ConversationBufferWindowMemory(k=1),
 )
+"""
 
 #Message handler for Slack
 @app.message(".*")
 def message_handler(message, say, logger):
     print(message)
     
-    output = chatgpt_chain.predict(human_input = message['text'])   
+    output = CHESSGPT.query(message)
     say(output)
+    
 
-
-
-# Start your app
+# Start 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
